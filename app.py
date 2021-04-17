1adc4445626191ca8613d154a003a65d57179bb6
KartexTiarc <christoffer.nyh@hotmail.com>
2021-04-17 12:30:05 +0000

    Added login and logout function

--- app.py
+++ app.py
@@ -47,12 +47,33 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("Welcome to recipe!")
+
     return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome")
+            else:
+                flash("incorrect")
+                return redirect(url_for("login"))
+                
     return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logout")
+    session.pop("user")
+    return redirect(url_for("login"))  
 
 
 if __name__ == "__main__":
