f8676254b5b134d2bd4530c8fa9867a993fdcde5
KartexTiarc <christoffer.nyh@hotmail.com>
2021-04-25 14:57:54 +0000

    Fixed the contact function

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,19 @@ $(document).ready(function(){
   });
 
 
+function sendMail(contactForm){
+    emailjs.send("service_sx70hpu","recipes", {
+        "from_first": contactForm.fname.value,
+        "from_lname": contactForm.lname.value,
+        "from_email": contactForm.email.value,
+        "question": contactForm.question.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error)
+        });
+        return false;
+        }
